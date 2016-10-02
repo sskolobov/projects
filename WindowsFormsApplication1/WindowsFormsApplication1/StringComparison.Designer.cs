71dfafb1d9cd10570c79b6b9057933decf516f53
sskolobov <s.s.kolobov@mail.ru>
2016-10-03 01:58:28 +0300

    Init

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/StringComparison.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace StringSimilarity
+{
+    partial class StringComparison
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Similarity = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Similarity
+            // 
+            this.btn_Similarity.Location = new System.Drawing.Point(73, 60);
+            this.btn_Similarity.Name = "btn_Similarity";
+            this.btn_Similarity.Size = new System.Drawing.Size(139, 75);
+            this.btn_Similarity.TabIndex = 2;
+            this.btn_Similarity.Text = "Сравнить";
+            this.btn_Similarity.UseVisualStyleBackColor = true;
+            this.btn_Similarity.Click += new System.EventHandler(this.btn_Similarity_Click);
+            // 
+            // StringComparison
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 214);
+            this.Controls.Add(this.btn_Similarity);
+            this.Name = "StringComparison";
+            this.Text = "Сравнение строк";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Similarity;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/StringComparison.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace StringSimilarity
+{
+    public partial class StringComparison : Form
+    {
+        public StringComparison()
+        {
+            InitializeComponent();
+        }
+
+        public static string PercentSimilarity(string str1, string str2)
+        {
+            if (str1 == "" || str2== "")
+            {
+                return "Одна из строк пуста!";
+            }
+            int diff;
+            int[,] m = new int[str1.Length + 1, str2.Length + 1];
+
+            for (int i = 0; i <= str1.Length; i++)
+                m[i, 0] = i;
+            for (int j = 0; j <= str2.Length; j++)
+                m[0, j] = j;
+            for (int i = 1; i <= str1.Length; i++)
+            {
+                for (int j = 1; j <= str2.Length; j++)
+                {
+                    diff = (str1[i - 1].ToString().ToLower() == str2[j - 1].ToString().ToLower()) ? 0 : 1;
+
+                    m[i, j] = Math.Min(Math.Min(m[i - 1, j] + 1,
+                                             m[i, j - 1] + 1),
+                                             m[i - 1, j - 1] + diff);
+                }
+            }
+            return (100 - ((100 * m[str1.Length, str2.Length]) / Math.Max(str1.Length, str2.Length))).ToString() + "%";
+        }
+
+        private void btn_Similarity_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Text format (*.txt)|*.txt";
+            fd.Title = "Выберите файл для сравнения";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    MessageBox.Show("Файл выбран", "Работа с файлами", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    StreamReader sr = new StreamReader(fd.FileName, Encoding.Default);
+                    StreamWriter sw = new StreamWriter(Path.GetDirectoryName(fd.FileName) +
+                        Path.GetFileNameWithoutExtension(fd.FileName) + "Similarity" + Path.GetExtension(fd.FileName),
+                        false, Encoding.Default);
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] arrStr = line.Split(',');
+                        arrStr[0] = arrStr[0].Trim();
+                        arrStr[1] = arrStr[1].Trim();
+                        sw.WriteLine(PercentSimilarity(arrStr[0], arrStr[1]));
+                    }
+                    sr.Close();
+                    sw.Close();
+                    if (MessageBox.Show("Сравнение слов в файле завершено.", "Работа с файлами", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                    {
+                        Process.Start(Path.GetDirectoryName(fd.FileName) +
+                        Path.GetFileNameWithoutExtension(fd.FileName) + "Similarity" + Path.GetExtension(fd.FileName));
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Работа с файлами", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
